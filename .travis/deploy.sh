18a4770b7463178d3077959f972af424ddbfbbaf
Santosh Manapragada <msantosh@amdocs.com>
2017-12-16 19:47:39 +0530

    Perform CI to Upload artifacts to Nexus

---
+++ .travis/deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+cd `dirname $0`/.. 
+
+if [ -z "$SONATYPE_USERNAME" ]
+then
+    echo "error: please set SONATYPE_USERNAME and SONATYPE_PASSWORD environment variable"
+    exit 1
+fi
+
+if [ -z "$SONATYPE_PASSWORD" ]
+then
+    echo "error: please set SONATYPE_PASSWORD environment variable"
+    exit 1
+fi
+
+if [ ! -z "$TRAVIS_TAG" ]
+then
+    echo "on a tag -> set pom.xml <version> to $TRAVIS_TAG"
+    mvn --settings .travis/settings.xml org.codehaus.mojo:versions-maven-plugin:2.1:set -DnewVersion=$TRAVIS_TAG 1>/dev/null 2>/dev/null
+else
+    echo "not on a tag -> keep snapshot version in pom.xml"
+fi
+
+mvn clean deploy --settings .travis/settings.xml -DskipTests=true -B -U
