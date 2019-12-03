1c9e7cca9acbc071927236d1836c234e4522e167
Adam Ness <adam.ness@gmail.com>
2019-12-03 10:09:17 -0600

    Bump dependency versions, enable Java 11 support

--- swagger2puml-core/src/main/java/com/kicksolutions/swagger/plantuml/PlantUMLGenerator.java
+++ swagger2puml-core/src/main/java/com/kicksolutions/swagger/plantuml/PlantUMLGenerator.java
@@ -2,47 +2,50 @@ package com.kicksolutions.swagger.plantuml;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import io.swagger.models.Swagger;
 import io.swagger.parser.SwaggerParser;
+import net.sourceforge.plantuml.GeneratedImage;
+import net.sourceforge.plantuml.SourceFileReader;
 
 /**
  * MSANTOSH
  *
  */
-public class PlantUMLGenerator 
+public class PlantUMLGenerator
 {
 	private static final Logger LOGGER = Logger.getLogger(PlantUMLGenerator.class.getName());
-	
+
 	public PlantUMLGenerator() {
 		super();
 	}
-	    
+
     /**
-     * 
+     *
      * @param specFile
      * @param output
      */
     public void transformSwagger2Puml(String specFile,String output,boolean generateDefinitionModelOnly,boolean includeCardinality,boolean generateSvg){
     	LOGGER.entering(LOGGER.getName(), "transformSwagger2Puml");
-    	
+
     	File swaggerSpecFile = new File(specFile);
     	File targetLocation = new File(output);
-    	
-    	if(swaggerSpecFile.exists() && !swaggerSpecFile.isDirectory() 
-    			&& targetLocation.exists() && targetLocation.isDirectory()) { 
-    		
+
+    	if(swaggerSpecFile.exists() && !swaggerSpecFile.isDirectory()
+    			&& targetLocation.exists() && targetLocation.isDirectory()) {
+
     		Swagger swaggerObject = new SwaggerParser().read(swaggerSpecFile.getAbsolutePath());
     		PlantUMLCodegen codegen = new PlantUMLCodegen(swaggerObject, targetLocation, generateDefinitionModelOnly, includeCardinality);
     		String pumlPath = null;
-    		
+
     		try{
     			LOGGER.info("Processing File --> "+ specFile);
-    			pumlPath = codegen.generatePuml();    		
+    			pumlPath = codegen.generatePuml();
     			LOGGER.info("Sucessfully Create PUML !!!");
-    			
+
     			if(generateSvg)
     			{
     				generateUMLDiagram(pumlPath, targetLocation);
@@ -55,18 +58,23 @@ public class PlantUMLGenerator
     	}else{
     		throw new RuntimeException("Spec File or Ouput Locations are not valid");
     	}
-    	
+
     	LOGGER.exiting(LOGGER.getName(), "transformSwagger2Puml");
     }
-    
+
     /**
-     * 
+     *
      * @param pumlLocation
      * @param targetLocation
      * @throws IOException
      * @throws InterruptedException
      */
     private void generateUMLDiagram(String pumlLocation,File targetLocation) throws IOException, InterruptedException{
-    	net.sourceforge.plantuml.Run.main(new String[]{"-tsvg","-o",targetLocation.getAbsolutePath(),"-I",pumlLocation});
+		File source = new File(pumlLocation);
+		SourceFileReader reader = new SourceFileReader(source);
+		List<GeneratedImage> list = reader.getGeneratedImages();
+		for( GeneratedImage image :list ) {
+			image.getPngFile().renameTo(new File(targetLocation, image.getPngFile().getName()));
+		}
     }
 }
